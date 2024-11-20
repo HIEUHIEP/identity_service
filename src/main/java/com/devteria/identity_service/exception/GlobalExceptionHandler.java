2f28143ec0d8282ae16a1544e59745d6e5f55ca4
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-20 22:04:05 +0700

    #3: Quan ly Exception tap trung, @ControllerAdvice

---
+++ src/main/java/com/devteria/identity_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.devteria.identity_service.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = RuntimeException.class)
+    ResponseEntity<String> handlingRunTimeException(RuntimeException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+
+    }
+}
