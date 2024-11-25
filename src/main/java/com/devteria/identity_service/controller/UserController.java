fb48969e1c3f7b981676d333eff8432694333c0d
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-25 22:32:28 +0700

    #19 UnitTest

--- src/main/java/com/devteria/identity_service/controller/UserController.java
+++ src/main/java/com/devteria/identity_service/controller/UserController.java
@@ -23,6 +23,7 @@ public class UserController {
 
     @PostMapping
     ApiResponse<UserResponse> createUser(@RequestBody @Valid UserCreationRequest request) {
+        log.info("Controller createUser");
         ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
         apiResponse.setResult(userService.createUser(request));
         return apiResponse;
