cd4e3a421335f2855708381b5a6030bd0df7dd83
LE HIEU HIEP <hieuhiep.ntqn88@gmail.com>
2024-11-26 22:49:09 +0700

    #21.1 Integration test voi TestContainers

---
+++ src/test/java/com/devteria/identity_service/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,96 @@
+package com.devteria.identity_service.controller;
+
+import com.devteria.identity_service.dto.request.UserCreationRequest;
+import com.devteria.identity_service.dto.response.UserResponse;
+import com.devteria.identity_service.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+public class UserControllerIntegrationTest {
+
+    @Container
+    static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>("mysql:latest");
+
+    @DynamicPropertySource
+    static void configureDataSource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driverClassName", () -> "com.mysql.cj.jdbc.Driver");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private UserCreationRequest request;
+    private UserResponse userResponse;
+    private LocalDate dob;
+
+    @BeforeEach
+        // Chạy trước mổi lần test.
+    void initData() {
+        dob = LocalDate.of(1990, 1, 1);
+        request = UserCreationRequest.builder()
+                .username("username")
+                .password("password")
+                .firstName("firstNameNew")
+                .lastName("lastNameNew")
+                .dob(dob)
+                .build();
+        userResponse = UserResponse.builder()
+                .id("123123123")
+                .username("username")
+                .firstName("firstName")
+                .lastName("lastName")
+                .dob(dob)
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        // WHEN => THEN
+        var res = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.username").value("username"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.firstName").value("firstNameNew"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.lastName").value("lastNameNew"));
+
+        log.info("Result: {}" + res.andReturn().getResponse().getContentAsString());
+        ;
+    }
+}
